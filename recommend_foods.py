fd7097039fd9bc0637ee27ab0ecc877e22a77ef4
Byeong-Hyeon Kim <daraha76@users.noreply.github.com>
2021-05-05 01:18:25 +0900

    Update recommend_foods.py: mapping similarity list for each foods

--- recommend_foods.py
+++ recommend_foods.py
@@ -13,4 +13,20 @@ wv_ingredient_loaded = KeyedVectors.load('./recipe_embedding/_model_ingredient_w
 menu2vec = menu_embedding.get_menu2vec(wv_ingredient_loaded)
 print(menu2vec.index_to_key) # menu2vec에 포함된 memu 목록
 
+food_freq = {} # Food Classifier에서 분류 결과로서 생성한 dictionory라고 가정함
+food_freq['닭갈비'] = 6
+food_freq['오일파스타'] = 4
+food_freq['김밥'] = 2
+food_freq['된장찌개'] = 1
+food_freq['쌀국수'] = 1 # '씰국수'는 menu2vec에 포함되어있지 않음!
 
+food_sim = {} # food_freq의 각 food당 유사한 음식들의 목록
+for food in food_freq:
+  if food not in menu2vec.index_to_key:
+    food_sim[food] = []
+  else:
+    food_sim[food] = menu2vec.most_similar(positive=[food], topn=20)
+
+for menu, sim in food_sim.items():
+  print(menu, end=' -> ')
+  print(sim)

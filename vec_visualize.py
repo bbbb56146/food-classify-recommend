c2bda747596e0c5eb60f60493932cae839fd9daa
Byeong-Hyeon Kim <daraha76@users.noreply.github.com>
2021-05-30 18:08:18 +0900

    Update vec_visualize.py

--- vec_visualize.py
+++ vec_visualize.py
@@ -4,26 +4,33 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import pandas as pd
-from gensim.models import KeyedVectors
+from IPython.display import display
 import os
 from recipe_embedding import menu_embedding
 
+# matplotlib 폰트관련 설정
 mpl.rcParams['axes.unicode_minus'] = False
-
-font_path = r'C:\Users\daraH\Downloads\NanumFontSetup_TTF_GOTHIC\NanumGothic.ttf'
+font_path = r'C:\Windows\Fonts\malgunsl.ttf'
 font_name = fm.FontProperties(fname=font_path).get_name()
 plt.rc('font', family=font_name)
 fontprop = fm.FontProperties(fname=font_path)
 
-def show_tsne():
-  tsne = TSNE(n_components=2, perplexity=10)
-  X = tsne.fit_transform(X_show)
 
-  df = pd.DataFrame(X, index=vocab_show, columns=['x', 'y'])
+def show_tsne(X_in, vocab):
+  tsne = TSNE(n_components=2, perplexity=10)
+  X = tsne.fit_transform(X_in)  # tsne로 차원 축소
+
+  df = pd.DataFrame(X, index=vocab, columns=['x', 'y'])
+
   fig = plt.figure()
   fig.set_size_inches(10, 10)
   ax = fig.add_subplot(1, 1, 1)
-  ax.scatter(df['x'], df['y'], s=S, c=C, label=Label)
+  for i in range(len(X_in)):
+    if Label_flag[Label[i]] == 0:
+      ax.scatter(df['x'][i], df['y'][i], s=S[i], c=C[i], label=Label[i])
+      Label_flag[Label[i]] = 1
+    else:
+      ax.scatter(df['x'][i], df['y'][i],  s=S[i], c=C[i])
 
   for word, pos in df.iterrows():
     ax.annotate(word, pos, fontproperties=fontprop, size=5)
@@ -31,9 +38,13 @@ def show_tsne():
   plt.legend()
   plt.xlabel("T-sne attribute 0")
   plt.ylabel("T-sne attribute 1")
-  plt.show(loc=(1.0, 1.0))
-  #plt.savefig('./menu2vec_tsne.png')
 
+  fig_tsne = plt.gcf()
+  plt.show(loc=(1.0, 1.0)) # plot을 출력
+  fig_tsne.savefig('./menu2vec_tsne.png') # plot을 이미지 파일로 저장
+
+
+# menu의 category 분류하기
 category = {}
 folders = os.listdir('./recipe_data')
 print(folders)
@@ -52,31 +63,40 @@ X = menu2vec[vocab]
 C = []
 S = []
 Label = []
+Label_flag = {}
+X_set = ['국_탕', '메인반찬', '면_만두', '밥_죽_떡', '빵', '양식', '찌개']
 for food in menu2vec.index_to_key:
   S.append(10)
   if food in category['국_탕']:
     C.append('violet')
-    Label.append('Set1')
+    Label.append(X_set[0])
+    Label_flag[X_set[0]] = 0
   elif food in category['메인반찬']:
     C.append('green')
-    Label.append('Set2')
+    Label.append(X_set[1])
+    Label_flag[X_set[1]] = 0
   elif food in category['면_만두']:
     C.append('blue')
-    Label.append('Set3')
+    Label.append(X_set[2])
+    Label_flag[X_set[2]] = 0
   elif food in category['밥_죽_떡']:
     C.append('black')
-    Label.append('Set4')
+    Label.append(X_set[3])
+    Label_flag[X_set[3]] = 0
   elif food in category['빵']:
     C.append('brown')
-    Label.append('Set5')
+    Label.append(X_set[4])
+    Label_flag[X_set[4]] = 0
   elif food in category['양식']:
     C.append('springgreen')
-    Label.append('Set6')
+    Label.append(X_set[5])
+    Label_flag[X_set[5]] = 0
   elif food in category['찌개']:
     C.append('red')
-    Label.append('Set7')
+    Label.append(X_set[6])
+    Label_flag[X_set[6]] = 0
 
 sz = 210
 X_show = X[:sz,:]
 vocab_show = vocab[:sz]
-show_tsne()
+show_tsne(X, vocab)
